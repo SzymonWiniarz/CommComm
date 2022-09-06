41df24728d6c9e7e6d541e1d8121c900bb0bcbb3
Szymon Winiarz <szwiniarz@gmail.com>
2022-09-06 23:27:10 +0200

    first commit

---
+++ src/main/java/pl/simcode/comm_comm/CommCommApplication.java
@@ -0,0 +1,13 @@
+package pl.simcode.comm_comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommCommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommCommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/simcode/comm_comm/CommCommApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.simcode.comm_comm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommCommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
